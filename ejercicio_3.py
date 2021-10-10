6bc45aa1bab570579dec5326749535f39f4dab32
Mirian <90347673+mirigonza@users.noreply.github.com>
2021-10-10 18:35:00 -0300

    Add files via upload

---
+++ ejercicio_3.py
@@ -0,0 +1,46 @@
+# Funciones [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 2.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejercicios con funciones
+
+# --------------------------------
+# Aquí dentro definir la función ordenar
+#def ordenar (numeros):
+
+
+# --------------------------------
+def ordenar (numeros):
+    numeros.sort()
+    resultado = numeros
+    return resultado
+
+if __name__ == '__main__':
+    print("Bienvenidos a otra clase de Inove con Python")
+    
+    numeros = [2, 4, 10, 8, 12, 6]
+
+    # Alumno: Crear la función "ordenar"
+
+    # Generar una una nueva funcion que se llame "ordenar",
+    # que utilizaremos para odernar la lista de numeros.
+    # Debe recibir 1 parámetro que es la lista de números
+    # y retornar la nueva lista ordenada (muy simular a la función promedio)
+
+    # Dentro de la función puede ordenar la lista
+    # usando la funciones nativas de Python "sort"
+
+    # Luego de crear la función invocarla en este lugar:
+
+    # lista_ordenada = ordenar(numeros)
+    lista_ordenada = ordenar(numeros)
+
+    # Imprimir en pantalla "lista_ordenada" que tendrá
+    # los valores retornado por la función ordenar:
+    print("lista ordenada", ordenar)
+    print("terminamos")
