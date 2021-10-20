7b8ad1e55017e536d61d59a28f2534c6368e7fea
Mirian <90347673+mirigonza@users.noreply.github.com>
2021-10-20 10:08:46 -0300

    Add files via upload

---
+++ ejercicio_5.py
@@ -0,0 +1,76 @@
+# Funciones [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 2.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejercicios con funciones y módulos
+import random
+
+# --------------------------------
+# Aquí dentro definir la función contar
+
+def contar(listas, numeros):
+    resultado = listas.count(numeros)
+    return resultado
+
+
+# Aquí copiar la función lista_aleatoria
+# ya elaborada
+
+def lista_aleatoria(inicio,fin,cantidad):
+    resultado = []
+    x = 0
+    for x in range(cantidad):
+        numero = random.randrange(inicio, fin+1)
+        resultado.append(numero)
+    return resultado
+# ----------------------------------
+
+
+if __name__ == '__main__':
+    print("Bienvenidos a otra clase de Inove con Python")
+    
+    inicio = 0
+    fin = 9
+    cantidad = 5
+
+    # Alumno: Crear la función "contar"
+
+    # Utilice la función "lista_aleatoria"  creado antes 
+    # para generar una lista de 5 números en
+    # un rango de 1 a 9 inclusive
+
+    # lista_numeros = lista_aleatoria(inicio, fin, cantidad)
+    lista_numeros = lista_aleatoria(inicio, fin, cantidad)
+    print(lista_numeros)
+        
+    # Generar una una nueva funcion que se llame "contar",
+    # que cuente la cantidad de veces que un elemento pasado
+    # por parámetro se repite en la lista también pasada por parámetro
+    
+    # Para saber cuantas veces se repiten el elemento pasado
+    # en la lista pueden usar el método nativo de list "count"
+
+    # Por ejemplo creo una lista de 5 elemtnos
+    
+    # Luego quiero averiguar cuantas veces se repite el numero 3
+    # cantidad_tres = contar(lista_numeros, 3)
+
+    # Luego de crear la función invocarla en este lugar:
+    # Averiguar cuantas veces se repite el numero 3
+
+    # cantidad_tres = contar(lista_numeros, 3)
+    cantidad_tres = contar(lista_numeros, 3)
+    
+
+    # Imprimir en pantalla "cantidad_tres" que informa
+    # cuantas veces se repite el tres en la lista
+
+    # print(cantidad_tres)
+    print(cantidad_tres)
+
+    print("terminamos")
